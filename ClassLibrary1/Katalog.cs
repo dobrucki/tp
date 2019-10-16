56fab72aefd240b981a3f32ad8cdecd3a473b8cd
wasilewskimateusz <wasilewskimateusz1998@gmail.com>
2019-10-17 00:09:31 +0200

    Classes: Wykaz, Zdarzenie added

---
+++ ClassLibrary1/Katalog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Katalog
+    {
+        private string _tytul;
+        private string _imieAutora;
+        private string _nazwiskoAutora;
+
+        public Katalog(string tytul, string imieAutora, string nazwiskoAutora)
+        {
+            _tytul = tytul;
+            _imieAutora = imieAutora;
+            _nazwiskoAutora = nazwiskoAutora;
+        }
+
+        public string Tytul { get; set; }
+        public string ImieAutora { get; set; }
+        public string NazwiskoAutora { get; set; }
+
+    }
+
+    
+}
---
+++ ClassLibrary1/OpisStanu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class OpisStanu
+    {
+        private Katalog _katalog;
+        private int _liczbaSztuk;
+
+        public OpisStanu(Katalog katalog, int liczbaSztuk)
+        {
+            _katalog = katalog;
+            _liczbaSztuk = liczbaSztuk;
+        }
+
+        public int LiczbaSztuk{ get; set; }
+        public Katalog Katalog { get; set; }
+    }
+}
