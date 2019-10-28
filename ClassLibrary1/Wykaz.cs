35441dc96f1689e2c2eb1f7d4e3fcd9ccaba3053
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-10-28 19:03:43 +0100

    Created Wykaz.Adres class, added Adres field into Wykaz class.

--- ClassLibrary1/Wykaz.cs
+++ ClassLibrary1/Wykaz.cs
@@ -8,15 +8,33 @@ namespace ClassLibrary1
 {
     public class Wykaz // Czytelnik
     {
+        public class Adres
+        {
+            public string Miasto { get; }
+            public string KodPocztowy { get; }
+            public string Ulica { get; }
+            public string Numer { get; }
+
+            public Adres(string miasto, string kodPocztowy, string ulica, string numer)
+            {
+                Miasto = miasto;
+                KodPocztowy = kodPocztowy;
+                Ulica = ulica;
+                Numer = numer;
+            }
+        }
+
+        public Guid IdWykazu { get; }
         public string FirstName { get; }
         public string LastName { get; }
-        public Guid IdWykazu { get; }
+        public Adres AdresWykazu { get; set; }
 
-        public Wykaz(string firstName, string lastName, Guid idWykazu)
+        public Wykaz(string firstName, string lastName, Guid idWykazu, Adres adres)
         {
             FirstName = firstName;
             LastName = lastName;
             IdWykazu = idWykazu;
+            AdresWykazu = adres;
         }
         
     }
