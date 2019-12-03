6c1898885d2369132ed60d765b66f5c2e13ead35
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-12-03 23:28:00 +0100

    Add JsonFormatter class
    -> Implemented Serialize method.

---
+++ Zadanie2/Zadanie2/JsonFormatter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Zadanie2
+{
+    class JsonFormatter
+    {
+        private static readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All,
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects
+        };
+
+        public static void Serialize(System.IO.Stream serializationStream, object graph)
+        {
+            string jsonText = JsonConvert.SerializeObject(graph, Formatting.Indented, _settings);
+            byte[] buffer = Encoding.UTF8.GetBytes(jsonText);
+            serializationStream.Write(buffer, 0, buffer.Length);
+        }
+
+        public static T Deserialize<T>(System.IO.Stream serializationStream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
