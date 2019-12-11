072c9c0742dcbca68825221319250397a212761a
wasilewskimateusz <wasilewskimateusz1998@gmail.com>
2019-12-11 20:08:10 +0100

    Stage 4 finished

---
+++ Zadanie3/Testy/Stage4Tests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zadanie3;
+using System.Linq;
+
+namespace Testy
+{
+    [TestClass]
+    public class Stage4Tests
+    {
+
+        [TestMethod]
+        public void GetProductsWithoutCategory_Declarative_Test()
+        {
+            using (ProductionDataContext db = new ProductionDataContext())
+            {
+                List<Product> products = db.GetTable<Product>().ToList();
+                List<Product> productListWithoutCategory = products.GetProductsWithoutCategory_Declarative();
+
+                foreach (Product product in productListWithoutCategory)
+                    Assert.IsNull(product.ProductSubcategory);
+            }
+        }
+
+
+
+        [TestMethod]
+        public void GetProductsWithoutCategory_Imperative_Test()
+        {
+            using (ProductionDataContext db = new ProductionDataContext())
+            {
+                List<Product> products = db.GetTable<Product>().ToList();
+                List<Product> productListWithoutCategory = products.GetProductsWithoutCategory_Imperative();
+
+                foreach (Product product in productListWithoutCategory)
+                    Assert.IsNull(product.ProductSubcategory);
+            }
+        }
+
+        [TestMethod]
+        public void SplitToPages_Declarative_Test()
+        {
+            using (ProductionDataContext db = new ProductionDataContext())
+            {
+                List<Product> products = db.GetTable<Product>().ToList();
+                List<Product> productPages = products.SplitToPages_Declarative(5, 3);
+
+                Assert.AreEqual(5, productPages.Count);
+                Assert.AreEqual(products[10].ProductID, productPages[0].ProductID);
+            }
+        }
+
+        [TestMethod]
+        public void SplitToPages_Imperative_Test()
+        {
+            using (ProductionDataContext db = new ProductionDataContext())
+            {
+                List<Product> products = db.GetTable<Product>().ToList();
+                List<Product> productPages = products.SplitToPages_Imperative(5, 3);
+
+                Assert.AreEqual(5, productPages.Count);
+                Assert.AreEqual(products[10].ProductID, productPages[0].ProductID);
+            }
+        }
+
+        [TestMethod]
+        public void ProductsAndVendorsToString_Declarative_Test()
+        {
+            using (ProductionDataContext db = new ProductionDataContext())
+            {
+                List<Product> products = db.GetTable<Product>().ToList();
+                List<ProductVendor> vendors = db.GetTable<ProductVendor>().ToList();
+
+                string output = products.ProductsAndVendorsToString_Declarative(vendors);
+                string firstline = output.Substring(0, output.IndexOf(Environment.NewLine));
+
+                
+                Assert.AreEqual("Adjustable Race-Litware, Inc.", firstline);
+            }
+        }
+
+        [TestMethod]
+        public void ProductsAndVendorsToString_Imperative_Test()
+        {
+            using (ProductionDataContext db = new ProductionDataContext())
+            {
+                List<Product> products = db.GetTable<Product>().ToList();
+                List<ProductVendor> vendors = db.GetTable<ProductVendor>().ToList();
+
+                string output = products.ProductsAndVendorsToString_Imperative(vendors);
+                string firstline = output.Substring(0, output.IndexOf(Environment.NewLine));
+
+               
+                Assert.AreEqual("Adjustable Race-Litware, Inc.", firstline);
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Zadanie3/Zadanie3/Stage4ExtensionMethods.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public static class Stage4ExtensionMethods
+    {
+        #region Declarative
+        public static List<Product> GetProductsWithoutCategory_Declarative(this List<Product> products)
+        {
+
+            IEnumerable<Product> output = from product in products
+                                          where product.ProductSubcategory == null
+                                          select product;
+
+            return output.ToList();
+
+        }
+
+        public static List<Product> SplitToPages_Declarative(this List<Product> products, int size, int pageNumber)
+        {
+            List<Product> output = (from product in products
+                                    select product).Skip((pageNumber - 1) * size).Take(size).ToList();
+            return output;
+        }
+
+        public static string ProductsAndVendorsToString_Declarative(this List<Product> products, List<ProductVendor> productVendors)
+        {
+
+            StringBuilder sB = new StringBuilder();
+
+            var pairs = from product in products
+                        from productVendor in productVendors
+                        where productVendor.ProductID == product.ProductID
+                        select new { Product = product.Name, Vendor = productVendor.Vendor.Name };
+
+            foreach (var pair in pairs)
+            {
+                sB.Append(pair.Product).Append("-").Append(pair.Vendor);
+                sB.AppendLine();
+            }
+            return sB.ToString();
+        }
+        #endregion
+
+        #region Imperative
+        public static List<Product> GetProductsWithoutCategory_Imperative(this List<Product> products)
+        {
+            return products.Where(p => p.ProductSubcategory == null).ToList();
+        }
+
+        public static List<Product> SplitToPages_Imperative(this List<Product> products, int size, int pageNumber)
+        {
+            return products.Skip((pageNumber - 1) * size).Take(size).ToList();
+        }
+
+        public static string ProductsAndVendorsToString_Imperative(this List<Product> products, List<ProductVendor> productVendors)
+        {
+
+            StringBuilder sB = new StringBuilder();
+
+            var pairs = products.Join(productVendors, product => product.ProductID, vendor => vendor.ProductID, (product, vendor)
+                 => new { Product = product.Name, Vendor = vendor.Vendor.Name });
+
+            foreach (var pair in pairs)
+            {
+                sB.Append(pair.Product).Append("-").Append(pair.Vendor);
+                sB.AppendLine();
+            }
+            return sB.ToString();
+        }
+
+        #endregion
+
+    }
+}
