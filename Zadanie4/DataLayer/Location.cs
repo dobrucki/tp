d2d11ae57f83c9792a98f546a0cb431d3bc14503
unknown <wasilewskimateusz1998@gmail.com>
2020-01-20 01:55:46 +0100

    Add main windows and ViewData

---
+++ Zadanie4/DataLayer/Location.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public partial class Location
+    {
+        public override string ToString()
+        {
+            return "Id: " + LocationID + " " + "Name: " + Name;
+        }
+
+        public string LocationToString
+        {
+            get => ToString();
+        }
+    }
+}
---
+++ Zadanie4/ViewData/ViewModel/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿using DataLayer;
+using ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using ViewData.MVVMLight;
+
+namespace ViewData.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            LoadDataCommand = new RelayCommand(() => DataLayer = new DataRepository());
+        }
+
+        public RelayCommand LoadDataCommand
+        {
+            get; private set;
+        }
+
+        public ObservableCollection<Location> Locations
+        {
+            get { return m_Locations; }
+            set
+            {
+                m_Locations = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Location Location
+        {
+            get
+            {
+                return m_Location;
+            }
+            set
+            {
+                m_Location = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public DataRepository DataLayer
+        {
+            get { return m_DataLayer; }
+            set
+            {
+                m_DataLayer = value;
+
+                Task.Run(() =>
+                {
+                    Locations = new ObservableCollection<Location>(value.GetAllLocations());
+                });
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private DataRepository m_DataLayer;
+        private ObservableCollection<Location> m_Locations;
+        private Location m_Location;
+        public int ID { get; set; }
+    }
+
+
+}
